52e57be81d1a0b1e52d265a69ebd18c3de198b1f
phantrunghieu91 <phantrunghieu1291@gmail.com>
2022-08-06 20:57:31 +0700

    timer

---
+++ hook/timer/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Timer from './components/Timer';
+
+function App() {
+  return (
+    <div className="timer-app">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
---
+++ hook/timer/src/components/Timer.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+function Timer(){
+    const [timer, setTimer] = useState(10);
+
+    useEffect(() => {
+        let id = setInterval(() => {timer > 0 && setTimer(timer - 1)}, 1000);
+        timer === 0 && alert('Time out!');
+        return () => {clearInterval(id)};
+    }, [timer]);
+
+    return (
+        <div className='timer'>
+            <h1 className='timer-display'>Counting down from {timer}</h1>
+        </div>
+    );
+}
+
+export default Timer;
