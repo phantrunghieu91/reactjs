6637822dcf501c649f145415071c8c9588b4d39f
phantrunghieu91 <phantrunghieu1291@gmail.com>
2022-08-14 21:13:24 +0700

    Contact form with formik (not run yet)

---
+++ react-form/contact-form/src/App.js
@@ -0,0 +1,117 @@
+import {useState} from 'react';
+import {Formik} from 'formik';
+import './App.scss';
+
+function App() {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  });
+
+  const REGEX = {
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    phone: /^([0-9]){10,11}$/
+  };
+
+  const handleChange = e => setForm({...form, [e.target.name]: e.target.value});
+
+  const handleValidate = () => {
+    const errors = {
+      name: '',
+      email: '',
+      phone: '',
+      message: ''
+    };
+    if(!form.name) {
+      errors.name = 'Required';
+    } 
+    if(!form.email) {
+      errors.email = 'Required';
+    }else if(!REGEX.email.test(form.email)){
+      errors.email = 'Invalid email address';
+    }
+    if(!form.phone) {
+      errors.phone = 'Required';
+    }else if(!REGEX.phone.test(form.phone)){
+      errors.phone = 'Invalid phone number';
+    }
+    if(!form.message) {
+      errors.message = "Required";
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = () => {
+    alert('Success sent message!');
+    //alert(JSON.stringify(form, null, 2));
+  };
+
+  return (
+    <div className="app">
+      <div className='app__header'>
+        <h1 className='app__header__title'>Contact Form</h1>
+      </div>
+      <div className='app__content'>
+        <Formik
+          initialValues={form}
+          validate={handleValidate}
+          onSubmit={handleSubmit}
+        >
+          {({errors, handleSubmit}) => (
+            <form 
+              className='app__content__form'
+              onSubmit={handleSubmit}
+            >
+              <div className={`app__content__form__input-group ${errors.name ? 'error' : ''}`}>
+                <label for='name'>Name</label>
+                <input 
+                  className='app__content__form__input-group__name'
+                  id='name' name='name' placeholder='Enter name here'
+                  value={form.name || ''} type='text'
+                  onChange={handleChange}
+                />
+                {<p className='app__content__form__input-group__error'>{errors.name}</p>}
+              </div>
+              <div className={`app__content__form__input-group ${errors.email ? 'error' : ''}`}>
+                <label for='email'>Email</label>
+                <input 
+                  className='app__content__form__input-email'
+                  id='email' name='email' placeholder='Enter email here'
+                  value={form.email || ''} type='email'
+                  onChange={handleChange}
+                />
+                {<p className='app__content__form__input-group__error'>{errors.email}</p>}
+              </div>
+              <div className={`app__content__form__input-group ${errors.phone ? 'error' : ''}`}>
+                <label for='phone'>Phone</label>
+                <input 
+                  className='app__content__form__input-phone'
+                  id='phone' name='phone' placeholder='Enter phone here'
+                  value={form.phone || ''} type='text'
+                  onChange={handleChange}
+                />
+                {<p className='app__content__form__input-group__error'>{errors.phone}</p>}
+              </div>
+              <div className={`app__content__form__input-group ${errors.message ? 'error' : ''}`}>
+                <label for='message'>Message</label>
+                <textarea 
+                  className='app__content__form__input-group__message'
+                  id='message' name='message' placeholder='Enter message here'
+                  value={form.message || ''}
+                  onChange={handleChange}
+                />
+                <p className='app__content__form__input-group__error'>{errors.message}</p>
+              </div>
+              <button className='app__content__form__submit' type='submit'>Submit</button>
+            </form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+}
+
+export default App;
