050df1efa96a13a2fa6459dcc7f85bf5dc308201
phantrunghieu91 <phantrunghieu1291@gmail.com>
2022-08-24 15:21:10 +0700

    added delete todo, toggle show/hide completed todo

--- client-api/exercises/pages/todos.js
+++ client-api/exercises/pages/todos.js
@@ -1,104 +1,158 @@
-import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import css from '../styles/Todos.module.css';
 import axios from 'axios';
+import React from 'react';
 
-const Todos = () => {
-    const [todos, setTodos] = useState([]);
+class Todos extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            todos: [],
+            todo: {},
+            showCompleted: true
+        }
+    }
 
-    const [todo, setTodo] = useState({});
-
-    const isObjectEmpty = (obj) => Object.keys(obj).length === 0;
-
-    const getTodos = async () => {
+    getTodos = async () => {
         return await axios.get(`https://jsonplaceholder.typicode.com/todos`);
     };
 
-    const addNewTodo = () => {
+    componentDidMount = () => {
+        if(this.state.todos.length === 0) {
+            this.getTodos()
+                .then(res => {
+                    this.setState({
+                        todos: res.data.filter(ele => ele.userId === 1)
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
+        }
+    };
+
+    addNewTodo = () => {
         axios
             .post('https://jsonplaceholder.typicode.com/todos', {
                 userId: 1,
-                id: todos.length + 1,
-                title: todo.title,
+                title: this.state.todo.title,
                 completed: false
             })
             .then(res => {
                 console.log(res);
-                getTodos()
+                this.setState({
+                    todos: [...this.state.todos, res.data]
+                });
+                this.setState({todo: {}});
             })
             .catch(err => {
                 throw err;
             })
     };
 
-    useEffect(() => {
-        if(todos.length === 0) {
-            getTodos()
-                .then(res => {
-                    setTodos(res.data.filter(ele => ele.userId === 1));
-                })
-                .catch(err => {
-                    throw err;
-                });
-        }
-    });
+    deleteTodo = (id) => {
+        axios
+            .delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                throw err;
+            })
+            .finally(() => {
+                this.setState(prevState => ({
+                    todos: prevState.todos.filter(ele => ele.id !== id)
+                }));
+            })
+        
+    };
 
-    const handleChange = (e) => {
+    handleChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        setTodo({...todo, [name]: value});
+        this.setState(prevState => {
+            const {todo} = prevState;
+            todo[name] = value;
+            return todo;
+        })
     };
 
-    const handleToggle = (index) => {
-        console.log(todos[index]);
-        const tempTodos = todos;
-        const modifyTodo = todos[index];
-        modifyTodo.completed = !modifyTodo.completed;
-        tempTodos[index] = modifyTodo; 
-        setTodos(tempTodos);
-        console.log(todos[index]);
+    handleToggle = (index) => {
+        this.setState((prevState) => {
+            return {
+                todos: prevState.todos.map((item, i) => {
+                    if (i === index) {
+                        return {
+                            ...item,
+                            completed: !prevState.todos[i].completed,
+                        }
+                    }
+                    return item;
+                })
+            }
+        })
     };
 
-    return (
-        <Layout title='Todo app'>
-            <div className={css.container}>
-                <header className={css.header}>
-                    <h1>Todo App</h1>
-                </header>
-                <main className={css.content}>
-                    <form className={css[`modify-form`]}
-                    onSubmit={() => {addNewTodo()}}>
-                        <header className={css[`form-header`]}>
-                            <h3>Add new todo</h3>
-                        </header>
-                        <main className={css[`form-content`]}>
-                            <input 
-                                type='text'
-                                name='title'
-                                placeholder='New todo here'
-                                value={todo.title || ''}
-                                onChange={handleChange}
-                            />
-                            <button type='submit'>Add</button>
-                        </main>
-                    </form>
-                    <div className={css[`display-todos`]}>
-                        <ul className={css[`todos-list`]}>
-                            {todos.map((item, index) => (
-                                <li 
-                                    className={css[`todo`]}
-                                    key={index}
-                                    onClick={() => handleToggle(index)}
-                                >
-                                    <span className={item.completed ? css[`todo-title`] : `${css[`todo-title`]} ${css[`completed`]}`}>{item.title}</span>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </main>
-            </div>
-        </Layout>
-    );
-};
+    handleShowHideCompleted = () => this.setState(prevState => ({ showCompleted: !prevState.showCompleted}));
+
+    render() {
+        const {todo, todos, showCompleted} = this.state;
+        return (
+            <Layout title='Todo app'>
+                <div className={css.container}>
+                    <header className={css.header}>
+                        <h1>Todo App</h1>
+                    </header>
+                    <main className={css.content}>
+                        <form className={css[`modify-form`]}
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            this.addNewTodo();
+                        }}>
+                            <header className={css[`form-header`]}>
+                                <h3>Add new todo</h3>
+                            </header>
+                            <main className={css[`form-content`]}>
+                                <input 
+                                    type='text'
+                                    name='title'
+                                    placeholder='New todo here'
+                                    value={todo.title || ''}
+                                    onChange={this.handleChange}
+                                />
+                                <button type='submit'>Add</button>
+                            </main>
+                        </form>
+                        <div className={css[`display-todos`]}>
+                            <ul className={css[`todos-list`]}>
+                            {
+                                todos.length > 0 && todos.map((item, index) => (
+                                    <li 
+                                        className={`${css['todo']} ${item.completed && !showCompleted ? css['hide'] : ''}`}
+                                        key={index}
+                                    >
+                                        <span 
+                                            className={!item.completed ? css[`todo-title`] : `${css[`todo-title`]} ${css[`completed`]}`}
+                                            onClick={() => this.handleToggle(index)}
+                                        >{item.title}</span>
+                                        <a className={css['delete-btn']} 
+                                            onClick={() => {this.deleteTodo(item.id)}}
+                                        />
+                                    </li>
+                                ))
+                            }
+                            </ul>
+                        </div>
+                        <div className={css[`button-group`]}>
+                            <button className={css[`toggle-completed`]}
+                                onClick={this.handleShowHideCompleted}
+                            >{showCompleted ? `Hide Completed` : `Show Completed`}</button>
+                        </div>
+                    </main>
+                </div>
+            </Layout>
+        );
+    }
+}
 
 export default Todos;
